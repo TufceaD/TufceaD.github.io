a43c4068d26560087467e200ffe8b0217a984e62
Daniel Tufcea <d.tufcea@gmail.com>
2015-01-13 13:57:44 -0500

    linked tabpane to tabs

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+//Toggle drop-down menu on hover
+	$(function() {
+        $(".dropdown").hover(
+            function(){ $(this).addClass('open') },
+            function(){ $(this).removeClass('open') }
+        );
+		
+		$(".dropdown").hover(
+            function(){ $(this).addClass('open') },
+            function(){ $(this).removeClass('open') }
+        );
+    });
+
+	
