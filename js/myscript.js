5afcd45d9fbb268b458c3b6c73082998a0c36efe
Daniel Tufcea <d.tufcea@gmail.com>
2015-01-13 23:29:21 -0500

    Settled for js with media query for hover nav

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,21 @@
 //Toggle drop-down menu on hover
-/*	$(function() {
+
+if (document.documentElement.clientWidth > 900) {
+	
+	$(function() {
         $(".dropdown").hover(
             function(){ $(this).addClass('open') },
             function(){ $(this).removeClass('open') }
         );
 		
     });
-*/
+}
 	
+	
+	$(function() {
+       $(".dropdown").not('.open').hover(
+           function(){ console.log($('.data-toggle', this));$('.data-toggle', this).dropdown('toggle') },
+           function(){}
+       );
+               
+   });
