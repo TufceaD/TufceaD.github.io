4a66f3fe88385dc8b0454bb8c39762d2a9f26bc1
Daniel Tufcea <d.tufcea@gmail.com>
2015-01-18 04:58:16 -0500

    Finished up about me section

--- js/myscript.js
+++ js/myscript.js
@@ -11,3 +11,22 @@ if (document.documentElement.clientWidth > 900) {
     });
 }
 	
+
+//Automatically update my age
+function age() {
+      var birthday = new Date(1990,7,28,0,0,0,0);
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+  return age;
+}
+
+$( document ).ready(function() {
+  $("#my_age").text( age() );
+});
+
+$( document ).ready(function(){
+$('a.custom-tab').click(function(){
+$('#myNavbar a[href="' + $(this).attr('href') + '"][data-toggle="tab"]').trigger('click');
+});
+ });
